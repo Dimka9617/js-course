20962db3e3b83456431c7c71df9b2bc4377394c7
Dima <jajaja100500@gmail.com>
2024-07-28 15:38:01 +0300

    dz3 img

--- lesson_03/additional1/js/additional1.js
+++ lesson_03/additional1/js/additional1.js
@@ -7,15 +7,15 @@ let emotionthree = 3
 let enterNumber = parseInt(prompt("Enter the number from 1 to 3", ''))
 if(enterNumber === emotionOne)
 	document.write(`
-	<img src="../img/smile/happy.jpg" alt="Happy smile">
+	<img src="img/happy.jpg" alt="Happy smile">
 	`)
 else if (enterNumber === emotiontwo)
 	document.write(`
-	<img src="../img/smile/sad.webp" alt="Sad smile">
+	<img src="img/sad.webp" alt="Sad smile">
 	`)
 else
 	document.write(`
-	<img src="../img/smile/angry.jpg" alt="Angry smile">
+	<img src="img/angry.jpg" alt="">
 	`)
 
 2
