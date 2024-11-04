997bc597d4e202a9aa9070b6312de4a3283752c6
Dima <jajaja100500@gmail.com>
2024-11-04 16:30:00 +0200

    dz 14 added

---
+++ lesson_13/task1/js/task1.js
@@ -0,0 +1,26 @@
+"use strict";
+// ------------------------
+// setTimeout(() => alert("Hello"),
+// 2000)
+
+// const timer = setTimeout(
+// 	greetingUser, 
+// 	2000,
+// 	'Hi', 
+// 	'Dima'
+// 	)
+
+// function greetingUser(greeting, name){
+// 	alert(`${greeting} ${name}`)
+// }
+
+// const askUser = prompt("Do u want to read the news ?")
+// let userAnswer = askUser
+// if(userAnswer === 'yes'){
+// 	window.location.href = 'https://www.ukr.net/'
+// }
+// else setTimeout(() =>
+// window.location.href = 'https://www.ukr.net/',
+// 20000
+// )
+
